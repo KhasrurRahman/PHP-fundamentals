b25a6f7dde060ac6d42ed66f208515bb5f9f1668
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2018-01-11 15:10:12 +0600

    PHP Constants

---
+++ PHP_Constants.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>javascript</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<section class="content">
+	<div class="hared"><h1>PHP FUNDAMENTAL BY RATIN</h1></div>
+
+
+	<section class="main">
+
+<?php
+		
+		define("value", "i am learning php", true);
+		echo VALUE."<br>";
+
+		function learnphp()
+		{
+			echo value;
+		}
+
+		learnphp();
+?>
+
+
+
+	</section>
+
+
+
+
+
+	<div class="footer" id="show">
+
+		<h2>this is footer</h2>
+	</div>
+
+</section>
+
+
+</body>
+</html>
