79f4dcb077e0dc20051c12b8d05eeb0961255521
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2018-01-13 01:59:23 +0600

    For Loops

---
+++ For_Loops.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>javascript</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<section class="content">
+	<div class="hared"><h1>PHP FUNDAMENTAL BY RATIN</h1></div>
+
+
+	<section class="main">
+
+<?php
+	//while:-
+	$a = 1;
+	while ($a <= 10) {
+		echo "the number is=$a <br> ";
+		$a++;//a=a+1
+	}
+
+	//do,while:-
+
+	$b = 5;
+	do//do always code ta age age kore nea pore while a jay
+	{
+		echo "the number is $b <br>";
+		$b++;
+	}
+
+	while ($b <= 10) 
+	
+
+
+?>
+
+
+
+	</section>
+
+
+
+
+
+	<div class="footer" id="show">
+
+		<h2>this is footer</h2>
+	</div>
+
+</section>
+
+
+</body>
+</html>
