09ce5579807a8b562ba15608c9d11873ba026425
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2018-01-13 01:12:51 +0600

    Switch Statements

---
+++ Switch_Statements.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>javascript</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<section class="content">
+	<div class="hared"><h1>PHP FUNDAMENTAL BY RATIN</h1></div>
+
+
+	<section class="main">
+
+<?php
+	$q = 5;
+	if($q <10)
+	{
+		echo "state is true";
+	}
+	elseif($q > 10)
+	{
+		echo "wrong1";
+	}
+	else
+	{
+		echo "nothing";
+	}
+
+?>
+
+
+
+	</section>
+
+
+
+
+
+	<div class="footer" id="show">
+
+		<h2>this is footer</h2>
+	</div>
+
+</section>
+
+
+</body>
+</html>
