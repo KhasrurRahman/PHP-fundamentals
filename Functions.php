0a271ebc401edfca0994faa953f585bb5dbcf0ee
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2018-01-13 02:16:26 +0600

    Functions

---
+++ Functions.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>javascript</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<section class="content">
+	<div class="hared"><h1>PHP FUNDAMENTAL BY RATIN</h1></div>
+
+
+	<section class="main">
+
+<?php
+	
+	$color = array("blue","skycolor","red","green");
+
+	foreach ($color as $c) //"forech normally string er somoy kaj kore"
+	{
+		echo "$c <br>";//here color er vator ja ja value passe ta sob "c" er vatore store kore print korsa
+	}
+	
+
+
+?>
+
+
+
+	</section>
+
+
+
+
+
+	<div class="footer" id="show">
+
+		<h2>this is footer</h2>
+	</div>
+
+</section>
+
+
+</body>
+</html>
