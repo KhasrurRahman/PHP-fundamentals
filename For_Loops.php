8fbd163e89fcac8f19ce78e1c60ec4fdab79dadf
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2018-01-13 02:07:41 +0600

    for loop

--- For_Loops.php
+++ For_Loops.php
@@ -13,23 +13,10 @@
 	<section class="main">
 
 <?php
-	//while:-
-	$a = 1;
-	while ($a <= 10) {
-		echo "the number is=$a <br> ";
-		$a++;//a=a+1
-	}
-
-	//do,while:-
-
-	$b = 5;
-	do//do always code ta age age kore nea pore while a jay
-	{
-		echo "the number is $b <br>";
-		$b++;
-	}
-
-	while ($b <= 10) 
+	
+	for ($i=0; $i <10 ; $i++) { 
+	 	echo "the number is= $i <br> ";
+	 } 
 	
 
 
